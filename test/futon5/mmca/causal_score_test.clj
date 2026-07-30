41d78bdce35568ec907eb56d8d061e4a0d2b3fdd
Joseph Corneli <jcorneli@brookes.ac.uk>
2026-07-30 10:34:33 +0100

    lock exact ordered causal calibration

--- test/futon5/mmca/causal_score_test.clj
+++ test/futon5/mmca/causal_score_test.clj
@@ -5,9 +5,10 @@
 (defn- fixed-field [rule]
   (vec (repeat 80 rule)))
 
-(deftest fixed-rule-zero-smoke-test
-  (testing "the standalone protocol runs and rule 0 has zero causal reach"
-    (is (= {:mean 0.0 :sd 0.0 :n 1 :seed-sd 0.0
-            :by-seed [{:seed 0 :damages [0] :mean 0.0}]}
-           (causal/reach (fixed-field 0) (causal/eca-config)
-                         {:seeds [0] :sites [0]})))))
+(deftest exact-ordered-calibration
+  (testing "rules 0, 204, and 90 reproduce the exact published invariants"
+    (doseq [[rule expected] [[0 0.0] [204 1.0] [90 8.0]]]
+      (let [result (causal/reach (fixed-field rule) (causal/eca-config))]
+        (is (= expected (:mean result)) (str "rule " rule))
+        (is (= 0.0 (:sd result)) (str "rule " rule " spread"))
+        (is (= 40 (:n result)) (str "rule " rule " sample count"))))))
